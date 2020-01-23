e800d0fcf6da7baca6255dda41528f6ced01d98a
hipho <yushark91@gmail.com>
2020-01-23 20:57:58 +0900

    a

---
+++ server/game.py
@@ -0,0 +1,102 @@
+"""
+Handles operations related to game and connections
+between, player, board, chat and round
+"""
+from .player import Player
+from .board import Board
+from .round import Round
+
+class Game():
+    def __init__(self, id, players, thread):
+        """
+        init the game! once the player threshold is met
+        :param id: int
+        :param players: Player[]
+        """
+        self.id = id
+        self.players = []
+        self.words_used = []
+        self.round = None
+        self.board = Board()
+        self.player_draw_ind = 0
+        self.connected_thread = thread
+        self.start_new_round()
+
+    def start_new_round(self):
+        """
+        Starts a new round with a new word
+        :return: None
+        """
+        self.round = Round(self.get_word(), self.players[self.player_draw_ind], self.players, self)
+        self.player_draw_ind += 1
+
+        if self.player_draw_ind >= len(self.players):
+            self.end_round()
+            self.end_game()
+
+    def player_guess(self, player, guess):
+        """
+        Makes the player guess the word
+        :param player: Player
+        :param guess: str
+        :return: bool
+        """
+        return self.round.guess(player, guess)
+
+    def player_disconnected(self, player):
+        """
+        Call to clean up objects when player disconnects
+        :param player: Player
+        :return: Exception()
+        """
+        pass
+
+    def skip(self):
+        """
+        Increment the round skips, if skips are greater than
+        threshold, starts new round
+        :return: None
+        """
+        if self.round:
+            new_round = self.round.skip()
+            if new_round:
+                self.round_ended()
+        else:
+            raise Exception("No round started yet")
+
+    def round_ended(self):
+        """
+        If the round ends call this
+        :return: None
+        """
+        self.start_new_round()
+        self.board.clear()
+
+    def update_board(self, x, y, color):
+        """
+        calls update method on board
+        :param x: int
+        :param y: int
+        :param color: tuple (int, int, int)
+        :return:
+        """
+        if not self.board:
+            raise Exception("No board created")
+        self.board.update(x,y,color)
+
+    def end_game(self):
+        """
+        ends the game
+        :return:
+        """
+        # todo implement
+        pass
+
+    def get_word(self):
+        """
+        gives a word that has not yet been used
+        :return: str
+        """
+        # todo get a list of words from somewhere
+        pass
+
---
+++ server/round.py
@@ -0,0 +1,93 @@
+"""
+Represents a round of the game, storing things like word, time, skips, drawing player and more.
+"""
+
+import time as t
+from _thread import *
+from .game import Game
+from .chat import Chat
+
+class Round():
+    def __init__(self, word, player_drawing, players, game):
+        """
+        init object
+        :param word: str
+        :param player_drawing: Player
+        :param players: Player[]
+        """
+        self.word = word
+        self.player_drawing = player_drawing
+        self.player_guessed = []
+        self.skips = 0
+        self.player_scores = {player: 0 for player in players}
+        self.time = 75
+        self.chat = Chat(self)
+        start_new_thread(self.time_thread, ())
+
+    def skip(self):
+        """
+        Returns true if round skipped threshold met
+        :return: bool
+        """
+        self.skips += 1
+        if self.skips > len(self.players) - 2:
+            return True
+        return False
+
+    def get_scores(self):
+        """
+        :returns: all the players scores
+        """
+        return self.scores
+
+    def get_score(self, player):
+        """
+        gets a specific player's scores
+        :param player: Player
+        :return: int
+        """
+        if player in self.player_scores:
+            return self.player_scores[player]
+        else:
+            raise Exception("Player not in score list")
+
+    def time_thread(self):
+        """
+        Runs in thread to keep track of time.
+        :return: None
+        """
+        while self.time > 0:
+            t.sleep(1)
+            self.time -= 1
+        self.end_round("Time is up")
+    def guess(self, player, wrd):
+        """
+        :returns bool if player got guess correct
+        :param player: Player
+        :param wrd: string
+        :return: bool
+        """
+        correct = wrd == self.word
+        if correct:
+            self.player_guessed.append(player)
+            # TODO implement scoring system here
+
+    def player_left(self, player):
+        """
+        removes a player that left from scores and list
+        :param player: Player
+        :return: None
+        """
+        # might not be able to use player as key in dict
+        if player in self.player_scores:
+            del self.player_scores[player]
+
+        if player in self.player_guessed:
+            self.player_guessed.remove(player)
+
+        if player == self.player_drawing:
+            self.end_round("Drawing player left")
+
+    def end_round(self, msg):
+        # TODO implement end round functionality
+        pass
